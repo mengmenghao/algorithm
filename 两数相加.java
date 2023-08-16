32eed824e0051c0a59f72e91affaa13b16dd929f
廖孟豪 <2860831154@qq.com>
2023-08-16 14:18:59 +0800

    2两数相加

--- 两数相加.java
+++ 两数相加.java
@@ -34,7 +34,59 @@
  * @createDate: 2023/8/13 23:15
  */
 public class 两数相加 {
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode listNode2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        ListNode listNode1 = addTwoNumbers(listNode, listNode2);
+
+        System.out.println(listNode1);
+    }
+
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // 定义一个供返回的节点head和 一个可移动的指针，用来指向存储两个数之和的位置
+        ListNode head = null, tail = null;
+        // 代表进位
+        int carry = 0;
+        // 第一和第二节点如果有一个不为空那就继续循环
+        while (l1 != null || l2 != null) {
+            // 如果节点不为空，代表节点值，否者为0
+            int n1 = l1 != null ? l1.val : 0;
+            // 如果节点不为空，代表节点值，否者为0
+            int n2 = l2 != null ? l2.val : 0;
+            // 计算这两个节点的和还加上了，上次计算的进位
+            int sum = n1 + n2 + carry;
+            // 如果head为空，代表着是第一次，需要初始化头节点和尾节点
+            if (head == null) {
+                // 因为节点中的值只能为个位数，那么，这里需要进行取模
+                head = tail = new ListNode(sum % 10);
+            } else {
+                // 将当前计算出的值放在尾部
+                tail.next = new ListNode(sum % 10);
+                // 让尾部等于尾部的下一个节点，修改当前节点的位置，即将tail指向此次新加的节点
+                tail = tail.next;
+            }
+            // 获取进位
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            // 如果最后的进位不为空，那么就在尾部把进位拼上
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+
 }
+
+
+
 
 class ListNode {
     int val;
@@ -50,5 +102,13 @@ class ListNode {
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
     }
 }
