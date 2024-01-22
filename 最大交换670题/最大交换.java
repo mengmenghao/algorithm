bd2cdd5ca22a999dce9f44a1f3cc8d8f549f6755
廖孟豪 <2860831154@qq.com>
2024-01-22 21:10:49 +0800

    最大交换

---
+++ 最大交换670题/最大交换.java
@@ -0,0 +1,73 @@
+package 最大交换670题;
+
+/**
+ * @author lmh
+ * @description:
+ * 给定一个非负整数，你至多可以交换一次数字中的任意两位。返回你能得到的最大值。
+ *
+ * 示例 1 :
+ *
+ * 输入: 2736
+ * 输出: 7236
+ * 解释: 交换数字2和数字7。
+ * 示例 2 :
+ *
+ * 输入: 9973
+ * 输出: 9973
+ * 解释: 不需要交换。
+ * 注意:
+ *
+ * 给定数字的范围是 [0, 108]
+ * @projectName: algorithm
+ * @className: 最大交换
+ * @createDate: 2024/1/22 20:54
+ */
+public class 最大交换 {
+    public static void main(String[] args) {
+
+    }
+
+    public int maximumSwap(int num) {
+        char[] charArray = String.valueOf(num).toCharArray();
+        int n = charArray.length;
+        int maxNum = num;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(charArray, i, j);
+                maxNum = Math.max(maxNum, Integer.parseInt(new String(charArray)));
+                swap(charArray, i, j);
+            }
+        }
+        return maxNum;
+    }
+
+    public int maximumSwapTwo(int num) {
+        char[] charArray = String.valueOf(num).toCharArray();
+        int n = charArray.length;
+        int maxIdx = n - 1;
+        int idx1 = -1, idx2 = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (charArray[i] > charArray[maxIdx]) {
+                maxIdx = i;
+            } else if (charArray[i] < charArray[maxIdx]) {
+                idx1 = i;
+                idx2 = maxIdx;
+            }
+        }
+        if (idx1 >= 0) {
+            swap(charArray, idx1, idx2);
+            return Integer.parseInt(new String(charArray));
+        } else {
+            return num;
+        }
+    }
+
+    public void swap(char[] charArray, int i, int j) {
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+    }
+}
+
+
+
